d6e8595417ea2df9faf5e1a4099a0a6b5a9a2019
Antony Premkumar <antonyashleyraja@gmail.com>
2021-11-25 07:48:18 +0100

    Stream to output a message

---
+++ src/include/streamer.h
@@ -0,0 +1,1 @@
+#define STREAM_BREAKER std::cout << "------------------------------" << std::endl
---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "include/streamer.h"
+
+int main()
+{
+    STREAM_BREAKER;
+    std::cout << "Jordan solves your CFD problem" << std::endl;
+    STREAM_BREAKER;
+
+
+    // create a rectangular mesh
+
+    // implement boundary conditions
+
+    // initialize temperature
+
+    // solve laplace equation for temperature
+
+    // log residuals
+
+    // print final results
+
+}
